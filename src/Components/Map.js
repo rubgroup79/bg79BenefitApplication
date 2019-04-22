4f02905c856484eefaf25416be93e3ace9afc58b
NoamSharon <46422735+NoamSharon@users.noreply.github.com>
2019-04-22 16:44:33 +0300

    sent suggestion

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -7,6 +7,7 @@ import Icon2 from 'react-native-vector-icons/AntDesign';
 import { Button, ButtonGroup } from 'react-native-elements';
 
 const { Marker } = MapView;
+
 _Latitude = 0;
 _Longitude = 0;
 
@@ -17,14 +18,14 @@ export class CallOutCard extends React.Component {
     super(props);
   }
 
-  sendSuggestion(ReceiverCode){
-    fetch('http://proj.ruppin.ac.il/bgroup79/test1/tar6/api/SendSuggestion?SenderCode='+this.props.SenderCode+'&ReceiverCode='+ReceiverCode, {
-      method: 'POST',
+  sendSuggestion(ReceiverCode) {
+    fetch('http://proj.ruppin.ac.il/bgroup79/test1/tar6/api/SendSuggestion?SenderCode=' + this.props.SenderCode + '&ReceiverCode=' + ReceiverCode, {
+      method: 'GET',
       headers: { "Content-type": "application/json; charset=UTF-8" },
-      body: JSON.stringify({}),
     })
       .then(res => res.json())
       .then(response => {
+        alert("Suggestion sent");
       })
       .catch(error => console.warn('Error:', error.message));
   }
@@ -32,47 +33,46 @@ export class CallOutCard extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, flexDirection: 'column', justifyContent: "center", alignItems: "center" }}>
-      <View style={{flex:2, flexDirection: 'column', justifyContent: "center"}}> 
-         <Image
-         source={{uri: this.props.Picture}}
-          //source={require("../../Images/TraineeAvatar.png")}
-          style={{ width: 35, height: 35 }}
-          /></View>
-         <View style={{flex:1, flexDirection: 'column', justifyContent:"center"}}>
-        <Text style={{fontSize: 10}}>{this.props.FirstName+" "+ this.props.LastName +","+ this.props.Age}</Text>
-        <Text style={{fontSize: 10}}>{(Math.floor(this.props.Distance * 10) / 10).toString() + ' KM away from you' }</Text>
+        <View style={{ flex: 2, flexDirection: 'column', justifyContent: "center" }}>
+          <Image
+            source={{uri: this.props.Picture}}
+            //source={require("../../Images/TraineeAvatar.png")}
+            style={{ width: 35, height: 35 }}
+          />
         </View>
-         <Button
-              title="Send Suggestion"
-              titleStyle={{ fontWeight: 'bold', fontSize: 10 }}
-              onPress={()=>{this.sendSuggestion(this.props.UserCode)} }
-              linearGradientProps={{
-                colors: ['#FF9800', '#F44336'],
-                start: [1, 0],
-                end: [0.2, 0],
-              }}
-              buttonStyle={{
-                borderWidth: 0,
-                borderColor: 'transparent',
-                borderRadius: 20,
-              }}
-              containerStyle={{ marginVertical: 5, height: 30, width: 110 }}
-              icon={{
-                name: 'arrow-right',
-                type: 'font-awesome',
-                size: 10,
-                color: 'white',
-              }}
-              iconRight
-              iconContainerStyle={{ marginLeft: 5, marginRight: -10 }}
-              
-            />
+        <View style={{ flex: 1, flexDirection: 'column', justifyContent: "center" }}>
+          <Text style={{ fontSize: 10 }}>{this.props.FirstName + " " + this.props.LastName + ", " + this.props.Age}</Text>
+          <Text style={{ fontSize: 10 }}>{(Math.floor(this.props.Distance * 10) / 10).toString() + ' KM away from you'}</Text>
+        </View>
+        <Button
+          title="Send Suggestion"
+          titleStyle={{ fontWeight: 'bold', fontSize: 10 }}
+          onPress={() => { this.sendSuggestion(this.props.ReceiverCode) }}
+          linearGradientProps={{
+            colors: ['#FF9800', '#F44336'],
+            start: [1, 0],
+            end: [0.2, 0],
+          }}
+          buttonStyle={{
+            borderWidth: 0,
+            borderColor: 'transparent',
+            borderRadius: 20,
+          }}
+          containerStyle={{ marginVertical: 5, height: 30, width: 110 }}
+          icon={{
+            name: 'arrow-right',
+            type: 'font-awesome',
+            size: 10,
+            color: 'white',
+          }}
+          iconRight
+          iconContainerStyle={{ marginLeft: 5, marginRight: -10 }}
+        />
       </View>
-
     )
   }
-
 }
+
 export default class LocationPage extends React.Component {
 
   constructor(props) {
@@ -111,11 +111,11 @@ export default class LocationPage extends React.Component {
           // title='my place:)'
           // description='here i am'
           >
-            <MapView.Callout 
+            <MapView.Callout
               tooltip={false}>
-               {/* style={{ flex: 1, position: 'absolute', width: 100, height: 80 }} */}
-                <CallOutCard SenderCode={'2'} ReceiverCode={'1'} FirstName={'Dana'} LastName={'godo'} Distance={'45'} Age={'25'} Picture={"../../Images/TraineeAvatar.png"}></CallOutCard>
-                    {/* <View style={{ flex: 1, flexDirection: 'column', justifyContent: "center", alignItems: "center" }}>
+              {/* style={{ flex: 1, position: 'absolute', width: 100, height: 80 }} */}
+              <CallOutCard SenderCode={29} ReceiverCode={30} FirstName={'Dana'} LastName={'godo'} Distance={'45'} Age={'25'} Picture={"../../Images/TrainerAvatar.png"}></CallOutCard>
+              {/* <View style={{ flex: 1, flexDirection: 'column', justifyContent: "center", alignItems: "center" }}>
 
             <Text>That's you</Text>
         <Image
@@ -137,12 +137,12 @@ export default class LocationPage extends React.Component {
               //  title={data.FirstName + ' ' + data.LastName + ', ' + data.Age.toString()}
               //  description={(Math.floor(data.Distance * 10) / 10).toString() + ' KM away from you'}
               //image={require('../assets/icon.png')}
-             
+
               >
-              <MapView.Callout>
-                <CallOutCard SenderCode={data.UserCode} FirstName={data.FirstName} LastName={data.LastName} Distance={data.Distance} Age={data.Age} Picture={data.Picture}></CallOutCard>
-              </MapView.Callout>
-            </MapView.Marker>
+                <MapView.Callout>
+                  <CallOutCard SenderCode={data.UserCode} FirstName={data.FirstName} LastName={data.LastName} Distance={data.Distance} Age={data.Age} Picture={data.Picture}></CallOutCard>
+                </MapView.Callout>
+              </MapView.Marker>
             )
             )}
           {this.props.groupResults == null ? null :
